05e76f41ddf465f61e21dbd5f7fc1e0c46878c3a
vbourassa <bourassa.v@outlook.com>
2019-10-08 16:16:07 +0200

    Ajout des recherches

--- myJavaness.js
+++ myJavaness.js
@@ -1,16 +1,6 @@
-addCSS('https://vbourassa.github.io/styles.css')
-addCSS('https://vbourassa.github.io/awesome.css')
-
-// Include CSS file
-function addCSS(filename){
- var head = document.getElementsByTagName('head')[0];
-
- var style = document.createElement('link');
- style.href = filename;
- style.type = 'text/css';
- style.rel = 'stylesheet';
- head.append(style);
-}
+$(document).ready(function() {
+    $('.tblManual').DataTable();
+} );
 
 // Dynamic tables
 /*!
